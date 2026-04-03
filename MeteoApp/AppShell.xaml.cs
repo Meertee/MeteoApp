438abdc1eb7c4c868dd06f62e7bb4cbe0a49966a
Meertee <vasco.pereira.silva@hotmail.com>
2026-04-03 23:50:51 +0200

    base setup

---
+++ MeteoApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Template
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MeteoApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Template
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
